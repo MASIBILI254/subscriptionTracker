9a1a49e41ff6cdfb02c83242be630b123ae81b41
MASIBILI254 <andreasimiyu7@gmail.com>
2026-09-09 10:33:32 +0300

    import routes to app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import {PORT} from './config/env.js'
+import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
 const app = express()
+app.use('/users', userRouter)
+app.use('/auth', authRouter)
+app.use('/subscriptions', subscriptionRouter)
 app.get('/', (req, res) => {
     res.send({body:'Welcome to subscription Tracker..'})
 })
